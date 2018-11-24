6f3332e83db4f99c7487c40739fd2f8914c94968
dasitu-dyc <lolicon945@gmail.com>
2018-11-24 14:43:33 +0800

    convert the cluster.txt for paraview

--- convert2vtu.py
+++ convert2vtu.py
@@ -5,7 +5,7 @@ infile = open('cluster.txt', 'r')
 n = int(infile.readline())
 x = np.array([0.0]*n)
 y = np.array([0.0]*n)
-z = np.array([0.0]*n)
+z = np.array([0.0]*n)    # it is a 2d structure, so left the z being 0.0
 value = np.zeros(n)
 for i in xrange(n) :
     line = infile.readline()

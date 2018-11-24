f2bd2cc849654552a732be2e11a760fc44522443
dasitu-dyc <lolicon945@gmail.com>
2018-11-24 14:18:11 +0800

    modify the get_initial_coordinates() to avoid overlay from the very begining.

---
+++ convert2vtu.py
@@ -0,0 +1,21 @@
+from evtk.hl import pointsToVTK
+import numpy as np
+
+infile = open('cluster.txt', 'r')
+n = int(infile.readline())
+x = np.array([0.0]*n)
+y = np.array([0.0]*n)
+z = np.array([0.0]*n)
+value = np.zeros(n)
+for i in xrange(n) :
+    line = infile.readline()
+    text = line.split()
+    x[i] = float(text[0])
+    y[i] = float(text[1])
+    value[i] = int(text[2])
+
+x = x - np.mean(x)
+y = y - np.mean(y)
+
+pointsToVTK('dlca', x, y, z, data = {"value" : value})
+infile.close()
